5ee6c62e4cab8b5a9d551b2d5567d42909874061
adriangerardraj <34865898+adriangerardraj@users.noreply.github.com>
2018-04-13 23:09:26 -0400

    Randomly selecting category for game: -- Accessing Firebase with JSON ( DataSnapshots) -- Implemented for way to make execution delayed

--- server.js
+++ server.js
@@ -1,20 +1,61 @@
 const http = require('http'); //for http protocol
 const firebase = require('firebase'); //for database
-var fs = require('fs');
+const fs = require('fs');
 const express = require('express');
-const app = express();
-const port = 3000;
-
-var Graph = require('graph-data-structure');
+const ArrayList = require('array-list');
+const Graph = require('graph-data-structure');
+const rn = require('random-number');
 
 var graph = Graph();
-
+const app = express();
+const port = 3000;
 firebase.initializeApp({
 //	credential:  firebase.credential.applicationDefault(),
-	databaseURL: 'https://link-it-252.firebaseio.com/link-it-252.json',
-
+	databaseURL: 'https://link-it-252.firebaseio.com',
 
 });
+var categoryName;
+var dbCategoryRef;
+
+var categRef = firebase.database().ref();
+
+var categList = [];
+var numCategs = 0;
+categRef.once("value")
+    .then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            var key = childSnapshot.key;
+            categList.push(key);
+            numCategs++;
+        });
+    });
+
+var gen = rn.generator({
+     integer: true
+});
+
+var interval = setInterval(function (){
+    categoryName = categList[gen(0,numCategs-1,true)];
+    console.log("Selected category: " + categoryName);
+    dbCategoryRef = firebase.database().ref(categoryName).orderByKey();
+    dbCategoryRef.once("value")
+        .then(function(snapshot) {
+            snapshot.forEach(function(childSnapshot) {
+                var key = childSnapshot.key;
+                if(categoryName == "Login") {
+                    console.log("That stuff is confidential..")
+                }
+                else
+                {
+                    var value = childSnapshot.val();
+                    console.log(key + "  " + value);
+                }
+            });
+        });
+    clearInterval(interval);
+},300);
+
+
 
 app.get('/',(request,response)=>{
     response.send('Using express');
