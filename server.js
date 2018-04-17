42bb3d95a886e4da8275851ce23e524b7ee28eb3
adriangerardraj <34865898+adriangerardraj@users.noreply.github.com>
2018-04-17 00:42:34 -0400

    Signup implemented   -- Need to figure out add to database   -- ref.push() creates new key, need to fix

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const ArrayList = require('array-list');
 const Graph = require('graph-data-structure');
 const rn = require('random-number');
 const path = require('path');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 /*==============================================================================================================
 
              Declaration and Initialization of Variables
@@ -49,22 +50,30 @@ categRef.once("value")
             Populating loginMap and getting randomly generated category map for user to play
 
  ================================================================================================================= */
+
+
+dbLoginRef = firebase.database().ref("Login").orderByKey();
+dbLoginRef.once("value")
+    .then(function(snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+            var key = childSnapshot.key;
+            loginMap.set(key, childSnapshot.val());
+        })
+    });
+
+
 var interval = setInterval(function (){
-    categoryName = categList[gen(0,numCategs-1,true)];
+    do {
+        categoryName = categList[gen(0, numCategs - 1, true)];
+    }while(categoryName === "Login");
     console.log("Selected category: " + categoryName);
     dbCategoryRef = firebase.database().ref(categoryName).orderByKey();
     dbCategoryRef.once("value")
         .then(function(snapshot) {
             snapshot.forEach(function(childSnapshot) {
                 var key = childSnapshot.key;
-                if(categoryName == "Login") {
-                    loginMap.set(key,childSnapshot.val());
-                }
-                else
-                {
                     var value = childSnapshot.val();
                     console.log(key + "  " + value);
-                }
             });
         });
     clearInterval(interval);
@@ -75,14 +84,36 @@ var html = fs.readFileSync('main.html');
 app.use(express.static(__dirname));
 
 app.get('/',(request,response)=>{
-    response.sendFile(path.join(__dirname +'/main.html'));
+    response.sendFile(path.join(__dirname +'/Login.html'))
 });
 
+app.post('/guess', function(req, res) {
 
-app.get('/hello',(request,reqponse,next) =>{
-    console.log('Test');
-   var variable = request.query.name;
-   response.json({'status' : 200, 'message-body' : 'success'});
+    res.json({'status' : 200, 'message-body' : 'success'});
+});
+
+app.post('/login/:username/:password', function(req, res) {
+    var status = checkSignIn(req.params.username, req.params.password);
+    if(status === false)
+    {
+        res.status(404);
+    }
+    else {
+        res.status(200);
+    }
+    res.send();
+});
+
+app.post('/signup/:username/:password', function(req, res) {
+    var status = checkSignUp(req.params.username, req.params.password);
+    if(status === false)
+    {
+        res.status(404);
+    }
+    else {
+        res.status(200);
+    }
+    res.send();
 });
 
 app.listen(port, (err) => {
@@ -92,3 +123,30 @@ app.listen(port, (err) => {
 
     console.log(`server is listening on ${port}`);
 });
+
+
+function checkSignIn(name, password) {
+    for (var entry of loginMap.entries()) {
+        if(entry[0] === name) {
+            if(entry[1] === password)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function checkSignUp(uname, password) {
+    for (var entry of loginMap.entries()) {
+        if(entry[0] === uname) {
+            return false;
+        }
+    }
+    var ref = firebase.database().ref();
+    var newUser = {user: uname,pass: password};
+    
+    loginMap.set(uname, password);
+    return true;
+
+}
