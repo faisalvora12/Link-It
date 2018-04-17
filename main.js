e101b20bc15c60bf4bbe2d7d5a17a8da24921ff9
Faisal Vora <faisalvora12.fv@gmail.com>
2018-04-17 01:02:23 -0400

    Added the springy which has a default node and can add new nodes from the text entered in textfield and connect them to default node

--- main.js
+++ main.js
@@ -1,16 +1,68 @@
-
 var songcount=0;
-var audio = new Audio('boko.mp3');
+var audio = new Audio('C:boko.mp3');
 var x;
+var springy
+var graph = new Springy.Graph();
+var graphJSON = {
+    "nodes": [
+        "Amphitryon",
+        "Alcmene",
+        "Iphicles",
+        "Heracles",
+        "Batman",
+        "BruceWayne",
+        "Spiderman",
+        "PeterParker",
+        "Batmobile",
+        "FordGT",
+    ],
+    "edges": [
+        ["Amphitryon", "Alcmene"],
+        ["Alcmene", "Amphitryon"],
+        ["Amphitryon", "Iphicles"],
+        ["Amphitryon", "Heracles"],
+        ["Batman", "BruceWayne"],
+        ["Batman", "Spiderman"],
+        ["Batman", "Batmobile"],
+        ["Batmobile", "FordGT"],
+        ["Spiderman", "PeterParker"],
+        ["Spiderman", "Batman"]
+    ]
+};
+var graphJSON2 = {
+    "nodes": [
+        "FordGT"
+    ],
+    "edges": [
+    ]
+};
 function myFunction() {
     document.getElementById("demo").innerHTML = "Hello World";
 }
+//This is where the default graph will be displayed
+
+//function defaultgraph() {
+window.addEventListener('load', function() {
+    jQuery(function(){
+
+        graph.loadJSON(graphJSON2);
+
+        springy = jQuery('#springydemo').springy({
+            graph: graph
+        });
+    });
+})
+//}
+
 function hint() {
     alert("I am an alert box!");
 }
+
 function enter() {
     x = document.getElementById("inputsm").value;
+    document.getElementById("inputsm").value=" ";
     document.getElementById("myLabel").innerHTML = x;
+
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
         if (request.readyState == XMLHttpRequest.DONE) {
@@ -19,14 +71,26 @@ function enter() {
     };
     request.open('POST', 'http://localhost:3000/hello', true);
     request.send(x);
-
+    jQuery(function(){
+        //var graph = new Springy.Graph();
+        //var node1 = graph.newNode({label: 'hello'});
+        //graph.loadJSON(graphJSON);
+        //graph.addNode(node1);
+        graph.addNodes(x);
+        //var node2 = graph.newNode({label: "Batman"});
+        //graph.newEdge(node1, node2);
+        graph.addEdges(['FordGT',x]);
+        var springy = jQuery('#springydemo').springy({
+            graph: graph
+        });
+    });
 
 }
 function music() {
-  		songcount=songcount+1;
-  		alert
- 		if((songcount%2)!=0)
-		audio.play();
-		else 
-		audio.pause();
+    songcount=songcount+1;
+    alert
+    if((songcount%2)!=0)
+        audio.play();
+    else
+        audio.pause();
 }
